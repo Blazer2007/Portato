75eaa6b7483a378944773fca6cace11ee35867f4
Blaze2007007 <2025138441@estudantes.ips.pt>
2026-04-28 22:40:35 +0100

    Prebab scale updates

--- Portato/Assets/Scripts/Microwave_Launch.cs
+++ Portato/Assets/Scripts/Microwave_Launch.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+using UnityEngine.UIElements;
 
 public class Microwave_Launch : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _microwaveSound;
+    [SerializeField] private PlayerController _playerController;
+    [SerializeField] private Rigidbody2D _playerRigidbody;
+
+    [SerializeField] private float launchForce = 30f;
+    [SerializeField] private float launchAngle = 45f;
+    [SerializeField] private Animation _microwaveAnimation;
     void Start()
     {
-        
+        if(_audioSource == null)
+            _audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void LaunchPlayer()
+    {
+        _playerController.enabled = true;
+        _playerRigidbody.bodyType = RigidbodyType2D.Dynamic;
+        _playerRigidbody.AddForce(Quaternion.Euler(0, 0, launchAngle) * Vector2.right * launchForce, ForceMode2D.Impulse);
+        _audioSource.PlayOneShot(_microwaveSound);
+        if (_microwaveAnimation != null)
+            _microwaveAnimation.Play();
     }
 }
